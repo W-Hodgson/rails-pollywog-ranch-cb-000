5c1709e46bde2716c23e96a58d69104594d24955
Will Hodgson <will.hodgson@current-rms.com>
2019-11-13 13:52:35 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922151507) do
+ActiveRecord::Schema.define(version: 20191113134851) do
 
   create_table "frogs", force: :cascade do |t|
-    t.string "name"
-    t.string "color"
+    t.string  "name"
+    t.string  "color"
+    t.integer "pond_id"
   end
 
   create_table "ponds", force: :cascade do |t|
@@ -23,8 +24,9 @@ ActiveRecord::Schema.define(version: 20140922151507) do
   end
 
   create_table "tadpoles", force: :cascade do |t|
-    t.string "name"
-    t.string "color"
+    t.string  "name"
+    t.string  "color"
+    t.integer "frog_id"
   end
 
 end
